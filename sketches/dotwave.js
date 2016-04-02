727b57e7442c0a282adc427db6cab6425f3c3300
Yang Su <yang@quip.com>
2016-04-02 14:57:37 -0700

    Moved sketch.js

--- sketches/dotwave.js
+++ sketches/dotwave.js
@@ -1,10 +1,10 @@
-var xspacing = 16;    // Distance between each horizontal location
-var theta = 0.0;      // Start angle at 0
-var amplitude = 10.0; // Height of wave
-var period = 20.0;   // How many pixels before the wave repeats
-var w;                // Width of entire wave
-var dx;               // Value for incrementing x
-var yvalues;  // Using an array to store height values for the wave
+const xspacing = 16;    // Distance between each horizontal location
+let theta = 0.0;      // Start angle at 0
+const amplitude = 10.0; // Height of wave
+const period = 20.0;   // How many pixels before the wave repeats
+let w;                // Width of entire wave
+let dx;               // Value for incrementing x
+let yvalues;  // Using an array to store height values for the wave
 
 function setup() {
   createCanvas(800, 400);
@@ -22,11 +22,11 @@ function draw() {
 function calcWave() {
   // Increment theta (try different values for
   // 'angular velocity' here
-  theta += 0.05;
+  theta += 0.5;
 
   // For every x value, calculate a y value with sine function
-  var x = theta;
-  for (var i = 0; i < yvalues.length; i++) {
+  let x = theta;
+  for (let i = 0; i < yvalues.length; i++) {
     yvalues[i] = sin(x) * amplitude;
     x += dx;
   }
@@ -36,7 +36,7 @@ function renderWave() {
   noStroke();
   fill(250);
   // A simple way to draw the wave with an ellipse at each location
-  for (var x = 0; x < yvalues.length; x++) {
+  for (let x = 0; x < yvalues.length; x++) {
     ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
   }
 }
