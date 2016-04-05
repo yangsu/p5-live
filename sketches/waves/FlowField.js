8a714553871385ba9beb441c71108c276c286bd2
Yang Su <yang@quip.com>
2016-04-05 07:54:35 -0700

    updates to waves

--- sketches/waves/FlowField.js
+++ sketches/waves/FlowField.js
@@ -27,21 +27,27 @@ class FlowField {
   }
 
   draw() {
-    this.update((direction, position) => {
-      const diff = direction.copy().div(2);
+    stroke(155);
+    this.update((direction, position, i, j) => {
+      const diff = direction.copy().setMag(this.arrowSize).div(2);
       const start = p5.Vector.sub(position, diff);
       // const start = position;
       const end = p5.Vector.add(position, diff);
-      strokeWeight(1);
+      // strokeWeight(1);
       // console.log(diff.mag());
-      const gray = map(diff.mag(), 0, 50, 255, 0);
+      // const gray = map(diff.magSq(), 0, maxArrowSize / 2, 50, 255);
+      const gray = map(p5.Vector.angleBetween(direction, new p5.Vector(1, 0)), 0, PI, 0, 255);
       stroke(gray);
       line(start.x, start.y, end.x, end.y);
-      strokeWeight(3);
+      // fill(gray);
+      // const startX = i * this.rowSize;
+      // const startY = j * this.colSize;
+      // rect(startX, startY, startX + this.rowSize, startY + this.colSize);
+      // strokeWeight(3);
       // stroke(0, 0, 255);
       // point(start.x, start.y);
       // stroke(0, 255, 0, gray);
-      point(end.x, end.y);
+      // point(end.x, end.y);
     });
   }
 }
--- sketches/waves/waves.js
+++ sketches/waves/waves.js
@@ -1,29 +1,48 @@
 const size = 800;
-const fieldDensity = 20;
+const fieldDensity = 40;
+const noiseMultiplier = 0.05;
 let field;
+let maxDist;
+let maxArrowSize;
+let x = 0;
+let y = 0;
+
 function setup() {
   createCanvas(size, size);
   field = new FlowField(fieldDensity, fieldDensity, (i, j) => {
-    return new p5.Vector(1, 1);
+    const theta = map(noise(i * noiseMultiplier, j * noiseMultiplier), 0, 1, 0, TWO_PI);
+    return new p5.Vector(cos(theta), sin(theta));
   });
+  maxDist = pow(dist(0, 0, width, height) / 2, 2);
+  const cellSize = size / fieldDensity;
+  maxArrowSize = dist(0, 0, cellSize, cellSize) / 2;
+  background(0);
 }
 
 function draw() {
-  fill(0, 10);
-  rect(0, 0, width, height);
+  // noiseDetail(3, 0.45);
+  background(0);
+  // fill(0, 10);
+  // rect(0, 0, width, height);
+  x += map(noise(frameCount * 0.05), 0, 1, -2, 2);
+  y += map(noise(frameCount * 0.05 + 10000), 0, 1, -2, 2);
+  field.update((direction, position, i, j) => {
+    const theta = map(noise((i + x * 2) * noiseMultiplier, (j + y * 2) * noiseMultiplier), 0, 1, 0, TWO_PI);
+    direction.set(cos(theta), sin(theta));
+  });
   field.draw();
 }
 
-function mouseMoved() {
-  const mouse = new p5.Vector(mouseX, mouseY);
-  if (mouse.magSq() > 0) {
-    field.update((direction, position) => {
-      const newDirection = p5.Vector.sub(mouse, position);
-      const mag = newDirection.mag();
-      newDirection
-        .normalize()
-        .mult(map(mag, 0, 100, 0, 20));
-      direction.set(newDirection);
-    });
-  }
-}
+// function mouseMoved() {
+//   const mouse = new p5.Vector(mouseX, mouseY);
+//   if (mouse.magSq() > 0) {
+//     field.update((direction, position) => {
+//       const newDirection = p5.Vector.sub(mouse, position);
+//       const mag = newDirection.magSq();
+//       newDirection
+//         .normalize()
+//         .mult(map(mag, 0, maxDist, 0, maxArrowSize));
+//       direction.set(newDirection);
+//     });
+//   }
+// }
